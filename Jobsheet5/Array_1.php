b85de3bcf9f86d71c376bd8b75382881c45a62e9
fasyadita <fasyadita1@gmail.com>
2024-09-24 21:11:25 +0700

    soal nomer 1

--- Jobsheet5/Array_1.php
+++ Jobsheet5/Array_1.php
@@ -8,9 +8,12 @@
         
         $Listdosen = ["Elok Nur Hamdana","Unggul Pemenang","Bagas Nugraha"];
 
-        echo $Listdosen[2] . "<br>";
-        echo $Listdosen[0] . "<br>";
-        echo $Listdosen[1] . "<br>";
+        for ($i=0; $i < 3 ; $i++) { 
+            echo $Listdosen[$i];
+        }
+        // echo $Listdosen[2] . "<br>";
+        // echo $Listdosen[0] . "<br>";
+        // echo $Listdosen[1] . "<br>";
 
         ?>
     </body>
