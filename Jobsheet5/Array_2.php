f7cd61a7466932869e9c6a7643e6d1a92d2a90d7
fasyadita <fasyadita1@gmail.com>
2024-09-24 22:02:03 +0700

    soal no 2

--- Jobsheet5/Array_2.php
+++ Jobsheet5/Array_2.php
@@ -3,19 +3,30 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type ="text/css" href="style.css"/>
          <title></title>       
     </head>
     <body>
-        <table>
+    <table>
+            <tr>
+                <th>Nama</th>
+                <th>Domisili</th>
+                <th>Jenis Kelamin</th>
+            </tr>
+
         <?php
         $Dosen = [
             'nama'          => 'Elok Nur Hamdana',
             'domisili'      => 'Malang',
             'Jenis_kelamin' => 'Perempuan'];
+
+            echo"<tr>";
+            echo "<td>". $Dosen['nama'] ."</td>";
+            echo "<td>". $Dosen['domisili'] ."</td>";
+            echo "<td>". $Dosen['Jenis_kelamin'] ."</td>";
+
+            echo"</tr>";
     
-        echo "Nama          : {$Dosen ['nama']}<br>";
-        echo "Domisili      : {$Dosen ['domisili']}<br>";
-        echo "Jenis Kelamin : {$Dosen ['Jenis_kelamin']}<br>";
         ?>
         </table>
     </body>
