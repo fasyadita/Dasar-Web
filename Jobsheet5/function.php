fd074192f117595246a253483a3ee9a2630b0d74
fasyadita <fasyadita1@gmail.com>
2024-09-24 22:02:13 +0700

    soal fungsi

---
+++ Jobsheet5/function.php
@@ -0,0 +1,11 @@
+<?php
+function perkenalan(){
+    echo "Assalamualaikum ";
+    echo "Perkenalkan, nama saya Fasya<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+//memanggil fungsi yang sudah dibuat 
+perkenalan();
+perkenalan();
+?>
