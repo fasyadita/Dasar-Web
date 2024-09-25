e6fc0786db081045dc45c5635a9c95d510804fc9
fasyadita <fasyadita1@gmail.com>
2024-09-25 13:56:24 +0700

    server

---
+++ Jobsheet5/global_server.php
@@ -0,0 +1,7 @@
+<?php 
+            // date_default_timezone_set("Asia/Jakarta");
+            // echo date ("h:i:sa");
+            echo '<pre>';
+            print_r($_SERVER);
+            echo '</pre>';
+        ?>
