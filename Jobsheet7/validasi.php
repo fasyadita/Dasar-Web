f77b6df793dcd85e5e23f0e578626c985cf438ec
fasyadita <fasyadita1@gmail.com>
2024-10-09 15:22:32 +0700

    memfalidasi

---
+++ Jobsheet7/validasi.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Form input dengan validasi </title>
+</head>
+
+<body>
+    <h1>Form input dengan validasi</h1>
+        <form method="POST" action="proses_validasi.php">
+            <label for="nama">nama : </label>
+            <input type="text" name="nama" id="nama" name="nama"><br><br>
+
+            <label for="email">email :</label>
+            <input type="email" name="email" id="email" name="email"><br><br>
+
+            <input type="submit" name="submit" value="submit">
+
+            <br>
+
+        </form>
+
+</body>
+
+</html>
