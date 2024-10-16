f74d83ea138ee301c15d172f181fc45cdd01278f
fasyadita <fasyadita1@gmail.com>
2024-10-16 14:14:56 +0700

    upload ajazxx

--- Jobsheet8/upload_ajax.php
+++ Jobsheet8/upload_ajax.php
@@ -6,9 +6,24 @@ if (isset($_FILES['file'])){
     $file_size = $_FILES['file']['size'];
     $file_tmp = $_FILES['file']['tmp_name'];
     $file_type = $_FILES['file']['type'];
-    @$file_ext = strtolower("" . end(explode('.',$_FILES['file']['name'] )) . "");
-    $extensions = array()
+    $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+    $extensions = array(".jpg", ".jpeg" , ".png");
+    
+    if (!in_array($file_ext, $extensions) === false) {
+        $errors[] = "Ekstensi file yang dizinkan adalah JPG, PNG, atau JPG.";
+    }
 
+    if ($file_size > 2097152) {
+        $errors[] = 'ukuran file tidak boleh lebih dari 2 mb';
+    }
+
+    if (empty($errors) == true) {
+        move_uploaded_file($file_tmp, "documents/" .$file_name);
+        echo "File berhasil diunggah";
+    } else {
+        echo implode(" ", $errors);
+    }
+    
 }
 
 ?>
