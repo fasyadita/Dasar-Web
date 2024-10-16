c7def60f26d7fefec24942af584e7f92ccd10249
fasyadita <fasyadita1@gmail.com>
2024-10-16 13:23:20 +0700

    multiupload

---
+++ Jobsheet8/form_multiupload.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Multi upload Dokumen</title>
+    </head>
+    <body>
+        <h2>Unggah Dokumen</h2>
+        <form action="proses_upload.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="files[]" multiple="multiple" accept=".jpg, .jpeg , .png"/> 
+            <!-- "jpg", "jpeg", "png", "gif" -->
+            <input type="submit" value="unggah" />
+        </form>
+    </body>
+</html>
